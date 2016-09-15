79cabb2d1854a9acfa41f02ddb1e190b3c2ea6f4
Junior Porfirio <juniorporfirio@gmail.com>
2016-09-15 08:14:30 -0300

    Tamarin-Forms
    Palestra Xamarin-Forms PUCC Campinas

---
+++ MacacosForms/MacacosForms/Model/Macaco.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace MacacosForms.Model
+{
+	public class Macaco
+	{
+		public string Apelido
+		{
+			get;
+			set;
+		}
+		public string Local
+		{
+			get;
+			set;
+		}
+		public string Imagem
+		{
+			get;
+			set;
+		}
+		public string Descricao
+		{
+			get;
+			set;
+		}
+
+	}
+}
+
